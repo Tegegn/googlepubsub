be08bec61f278a527d52beacbc36f4459b098399
Tegegn <tegegngelan@gmail.com>
2018-04-07 09:56:05 -0400

    created beans and controller logic

--- src/main/java/com/techli/googlepubsub/GooglepubsubApplication.java
+++ src/main/java/com/techli/googlepubsub/GooglepubsubApplication.java
@@ -1,12 +1,72 @@
 package com.techli.googlepubsub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
+import org.springframework.cloud.gcp.pubsub.integration.AckMode;
+import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.cloud.gcp.pubsub.support.GcpHeaders;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
+@Slf4j
 public class GooglepubsubApplication {
+	Logger logger = LoggerFactory.getLogger(GooglepubsubApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GooglepubsubApplication.class, args);
 	}
+	
+	  @Bean
+	  public PubSubInboundChannelAdapter messageChannelAdapter(
+	      @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+	      PubSubOperations pubSubTemplate) {
+	    PubSubInboundChannelAdapter adapter =
+	        new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
+	    adapter.setOutputChannel(inputChannel);
+	    adapter.setAckMode(AckMode.MANUAL);
+
+	    return adapter;
+	  }
+	  
+	  @Bean
+	  public MessageChannel pubsubInputChannel() {
+	    return new DirectChannel();
+	  }
+	  
+	  @Bean
+	  @ServiceActivator(inputChannel = "pubsubInputChannel")
+	  public MessageHandler messageReceiver() {
+	    return message -> {
+	    	logger.info("Message arrived! Payload: " + message.getPayload());
+	      AckReplyConsumer consumer =
+	          (AckReplyConsumer) message.getHeaders().get(GcpHeaders.ACKNOWLEDGEMENT);
+	      consumer.ack();
+	    };
+	  }
+	  @Bean
+	  @ServiceActivator(inputChannel = "pubsubOutputChannel")
+	  public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+	    return new PubSubMessageHandler(pubsubTemplate, "testTopic");
+	  }
+	  
+	  @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+	  public interface PubsubOutboundGateway {
+
+	    void sendToPubsub(String text);
+	  }
 }
--- src/main/java/com/techli/googlepubsub/controller/SubscriberController.java
+++ src/main/java/com/techli/googlepubsub/controller/SubscriberController.java
@@ -1,16 +1,31 @@
 package com.techli.googlepubsub.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.techli.googlepubsub.GooglepubsubApplication.PubsubOutboundGateway;
 
 @RestController
 @RequestMapping("/html")
 public class SubscriberController {
 	
+	  @Autowired
+	  private PubsubOutboundGateway messagingGateway;
+	
 	@RequestMapping(value = "/pullMessage", method = RequestMethod.GET)
 	public String pullMessagefromTopic() {
 		return "Message pulling successfully";
 	}
+	
+	@PostMapping("/publishMessage")
+	  public RedirectView publishMessage(@RequestParam("message") String message) {
+	    messagingGateway.sendToPubsub(message);
+	    return new RedirectView("/");
+	  }
 
 }
